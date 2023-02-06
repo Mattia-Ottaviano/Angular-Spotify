ea62caf04000c4b726b63061ec6e80044617a75b
Mattia-Ottaviano <ottaviano.mattia@iisgalvanimi.edu.it>
2023-02-06 10:48:53 +0000

    .

--- first-routed-app/src/app/spotify.service.ts
+++ first-routed-app/src/app/spotify.service.ts
@@ -1,24 +1,52 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 //Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
 @Injectable({
   providedIn: 'root'  
 })
 export class SpotifyService {
-   //url per oauth: https://developer.spotify.com/console/get-search-item/
+  spotifyServiceObs: any;
+  service: any;
+  track: any;
+  //url per oauth: https://developer.spotify.com/console/get-search-item/
   //Ottengo il modulo HttpClient
   constructor(private http: HttpClient) { }
 
+  
+
+
   searchTrack(query: string) {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQC47f6Vq21Yw9JRMeygUq0JlBX2gApmdHwcd-IBtEpD7wA5WyTc0D6x05WcupOmq94XFdoF_iI1TbZZht-3CD-Z_Deu8OeF3FKm-KLQCpIEwVxEJlWg3kW6IAzwkpxnIRQi7n8HZtxLSx-di3a7yuhZwqp3DNjk7b3Wb6bQbywC25uxZmZnqrtGRp9dFgJqGgNa'
-    });
-
+    const headers = new HttpHeaders({Authorization: environment.oauthToken});
     let obsTracks = this.http.get(url, { headers });
     return obsTracks;
  //Ritorno un observable ai componenti che richiedono il servizio
   }
+  
+
+  getTrack(id: string): Observable<any> {
+    const url = `https://api.spotify.com/v1/tracks/${id}`;
+    const headers = new HttpHeaders({Authorization: environment.oauthToken});
+    
+    return this.http.get(url, { headers });
+  }
+
+
+   //Ogni volta che viene invocata la route tracks/:id, l'observable richiama questo metodo
+   getRouterParam = (params: ParamMap) =>
+   {
+     let trackId = params.get('id'); //Ottengo l'id dai parametri
+     console.log (trackId); //Stampo su console
+     //spotifyServiceObs va dichiarato
+     this.spotifyServiceObs = this.service.getTrack(trackId) ;
+     this.spotifyServiceObs.subscribe((data: any)=>this.track = data)
+   }
+
+   
+
 }
+
---
+++ first-routed-app/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    oauthToken: "Bearer BQC_PpAVFuUwIT4z6vCVJT5fnuNwH5hdSXtCEr-nor64vXndYmdgb_VSry1RrxKMLS-Blesopt8QWk2_Jny8_EiolRE2gDld4Wrn0-xwgjq282OVqC5XcXuFPBUiEKOkUm-FDjuhq6ezYATCQritoBQEdDoFKjIAALnXmwQ6KeeHxbj-6a1Jba4GsDQZaD2usPvG"
+  };
